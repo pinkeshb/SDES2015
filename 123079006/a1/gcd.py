a36dca72ad2120cd32db2c8be000ad18d10052b6
Pinkesh Barsopia <barsopia@gmail.com>
2015-02-15 00:26:44 +0530

    Adding few more tests to "test_gcd()"

--- 123079006/a1/gcd.py
+++ 123079006/a1/gcd.py
@@ -3,10 +3,13 @@ def gcd(a,b):
 		raise ValueError
 	if b<0:
 		raise ValueError
-	if not( (type(a) is int) or (type(a) is long) ):
+	if not(type(a) in [int ,   long ] and type(b) in [int, long ]):
 		raise TypeError
-	if not( (type(b) is int) or (type(b) is long) ):
-		raise TypeError
-	if b==0:
-		return a
-	return gcd(b,a%b)
+	if a == 0:
+		return b
+	while b != 0:
+		if a > b:
+			a = a - b
+		else:
+			b = b - a
+	return a
--- 123079006/a1/test_gcd.py
+++ 123079006/a1/test_gcd.py
@@ -1,51 +1,72 @@
 from gcd import gcd
 
 def test_gcd():
-	assert gcd(48, 72)==24
-	assert gcd(12, 13)==1
+	assert gcd(48, 72)==24 , "fails---gcd(48, 72) != 24"
+	assert gcd(12, 13)==1,"fails---gcd(12, 13)!=1"
 
 	flag=0
 	try:
 		gcd(-1,5)
 	except ValueError:
 		flag=1
-	assert flag==1,"ValueError not catched"
+	assert flag==1,"ValueError not catched gcd(-1,5)"
 
 	flag=0
 	try:
 		gcd(1,-5)
 	except ValueError:
 		flag=1
-	assert flag==1,"ValueError not catched"
+	assert flag==1,"ValueError not catched gcd(1,-5)"
+
+	flag=0
+	try:
+		gcd(-10,-5)
+	except ValueError:
+		flag=1
+	assert flag==1,"ValueError not catched gcd(-10,-5)"
 
 	flag=0
 	try:
 		gcd(1,0.5)
 	except TypeError:
 		flag=1
-	assert flag==1,"TypeError not catched"
+	assert flag==1,"TypeError not catched gcd(1,0.5)"
+
+	flag=0
+	try:
+		gcd(1,"sdf")
+	except TypeError:
+		flag=1
+	assert flag==1,"TypeError not catched gcd(1,'sdf')"
 
 	flag=0
 	try:
 		gcd(1.0,5)
 	except TypeError:
 		flag=1
-	assert flag==1,"TypeError not catched"
+	assert flag==1,"TypeError not catched gcd(1.0,5)"
 
 	flag=0
 	try:
 		gcd(1.0,'a')
 	except TypeError:
 		flag=1
-	assert flag==1,"TypeError not catched"
+	assert flag==1,"TypeError not catched gcd(1.0,'a')"
 
 	flag=0
 	try:
 		gcd('sdfklaldf',5)
 	except TypeError:
 		flag=1
-	assert flag==1,"TypeError not catched"
+	assert flag==1,"TypeError not catched gcd('sdfklaldf',5)"
+
+	flag=0
+	try:
+		gcd('sdfklaldf',5.0)
+	except TypeError:
+		flag=1
+	assert flag==1,"TypeError not catched gcd('sdfklaldf',5.0)"
 	
-	print "gcd passed all tests!"
+	print "gcd() passed all tests!"
 if __name__ == "__main__":
 	test_gcd()
