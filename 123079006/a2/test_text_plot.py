368743c2f46c3d3a187b23fbdd8a8897873243bf
Pinkesh Barsopia <barsopia@gmail.com>
2015-02-16 01:08:25 +0530

    Adding test cases for plot() and autorange_data(), code for plot()  autorange_data() and print_sine()

--- 123079006/a2/test_text_plot.py
+++ 123079006/a2/test_text_plot.py
@@ -1,13 +1,23 @@
 import text_plot
+import sys
+import filecmp
 
 def test_init_blank_plot_with_axis():
    assert text_plot.init_blank_plot_with_axis(80,30)==([' ']*39+['|']+[' ']*40+['\n'])*14  + (['-']*39+['+']+['-']*40+['\n']) +([' ']*39+['|']+[' ']*40+['\n'])*15,"init_blank_plot_with_axis(80,30) improper"
    assert text_plot.init_blank_plot_with_axis(121,31)==([' ']*60+['|']+[' ']*60+['\n'])*15  + (['-']*60+['+']+['-']*60+['\n']) +([' ']*60+['|']+[' ']*60+['\n'])*15,"init_blank_plot_with_axis(121,31) improper"
 
-def test_autorange_data(x,y,X_range,Y_range):
-   pass
-
-
+def test_autorange_data():
+   assert text_plot.autorange_data([],[],80,30)==([],[]),"autorange without data"
+   assert text_plot.autorange_data([0],[0],80,30)==([0],[0]),"autorange with (0,0)"
+   assert text_plot.autorange_data([30],[10],80,30)==([39],[13]),"autorange with (30,10) data"
+   assert text_plot.autorange_data([5],[5],11,11)==([5],[5]),"autorange wuth (5,5) data"
+   assert text_plot.autorange_data([-10],[10],11,11)==([-5],[5]),"autorange with (10,10) data"
+   assert text_plot.autorange_data([10.9],[-10.9],11,11)==([5],[-5]),"autorange with (10.9,10.9) data"
+   assert text_plot.autorange_data([-4.5],[10],11,11)==([-2],[5]),"autorange with (4.5,10) data"
+   assert text_plot.autorange_data([-10,0,-15.5],[10,0,-10.5],11,11)==([-3,0,-5],[3,0,-3]),"autorange with [-10,0,-15.5],[10,0,-10.5] data"
+   assert text_plot.autorange_data([10],[-10],10,20)==([4],[-4]),"autorange with (10,-10) data"
+   assert text_plot.autorange_data([1,0,4.3,5.4],[-1,0,8.5,3.1],10,20)==([1,0,3,4],[-1,0,6,2]),"autorange with [1,0,4.3,5.4],[-1,0,8.5,3.1] data"
+   assert text_plot.autorange_data((100,4,2),[100,4,1],10,20)==([4,0,0],[4,0,0]),"autorange with [100,4,2],[100,4,1] data"
 def test_rastering():
    assert text_plot.rastering([],[],80,30,text_plot.init_blank_plot_with_axis(80,30)) == text_plot.init_blank_plot_with_axis(80,30),"rastering() failed no data plot"
    
@@ -37,9 +47,15 @@ def test_rastering():
    plotlist[5*12+10-1]='*'
    assert text_plot.rastering([0,4.3333333],[0,0],11,11,text_plot.init_blank_plot_with_axis(11,11)) == plotlist,"rastering() failed multi-floating-point plot"
 
-def test_plot(x,y,X_range,Y_range):
-   pass
+def test_plot():
    #test for default size and nothing to plot
+   s=sys.stdout
+   sys.stdout = open('testcase_output_of_plot.txt', 'w')
+   text_plot.plot([0.5,-3.5,0,-3.8,2.1],[5,-1,0,+2.5,-1.9],50,55) 
+   sys.stdout=s
+   assert filecmp.cmp('expected_output_of_plot.txt','testcase_output_of_plot.txt'),"plot not working as expected"
+
+
 
 #test for default size and rastering single point
 
@@ -79,8 +95,8 @@ def test_plot(x,y,X_range,Y_range):
 def test_text_plot():
    test_init_blank_plot_with_axis()
    test_rastering()
-   #test_autorange_data()
-   #test_plot()
+   test_autorange_data()
+   test_plot()
    print "Good JOB"
 
 test_text_plot()
--- 123079006/a2/text_plot.py
+++ 123079006/a2/text_plot.py
@@ -1,3 +1,5 @@
+import math
+import time
 def init_blank_plot_with_axis(X_range,Y_range):
 	if X_range%2 == 1:
 		blank_line=[' ']*(X_range/2)+['|']+[' ']*(X_range/2)+['\n']
@@ -10,8 +12,30 @@ def init_blank_plot_with_axis(X_range,Y_range):
 	else:
 		blank_plot_with_axis=blank_line*((Y_range-1)/2)+blank_line_middle+blank_line*(Y_range/2)
 	return blank_plot_with_axis
+
 def autorange_data(x,y,X_range,Y_range):
-	pass
+	no_of_point=len(x)
+	if no_of_point==0:
+		return ([],[])
+	max_x=x[0]
+	max_y=y[0]
+	for i in range(0,no_of_point):
+		if max_x<abs(x[i]):
+			max_x=abs(x[i])
+		if max_y<abs(y[i]):
+			max_y=abs(y[i])
+	if max_x==0:
+		max_x=(X_range-1)/2
+	if max_y==0:
+		max_y=(Y_range-1)/2
+	scaling_factor = 1.0/max([float(max_x)/((X_range-1)/2),float(max_y)/((Y_range-1)/2)])
+	x_scaled=[]
+	y_scaled=[]
+	for i in range(0,no_of_point):
+		x_scaled.append(round(x[i]*scaling_factor))
+		y_scaled.append(round(y[i]*scaling_factor))
+	return (x_scaled,y_scaled)
+
 def rastering(x_scaled,y_scaled,X_range,Y_range,plot_list):
 	no_of_point=len(x_scaled)
 	if no_of_point==0:
@@ -22,5 +46,19 @@ def rastering(x_scaled,y_scaled,X_range,Y_range,plot_list):
 		plot_list[row*(X_range+1)+column]='*'
 	return plot_list
 
-def plot(x,y,X_range,Y_range):
-	pass
+def plot(x,y,X_range=80,Y_range=30):
+	Blank_plot_with_axis=init_blank_plot_with_axis(X_range,Y_range)
+	(x_scaled,y_scaled)=autorange_data(x,y,X_range,Y_range)
+	plot_list=rastering(x_scaled,y_scaled,X_range,Y_range,Blank_plot_with_axis)
+	print "".join(plot_list)
+
+def print_sine():
+	x=[0]
+	y=[0]
+	for i in range(1,31):
+		x.append(x[i-1]+2*math.pi/30)
+		y.append(math.sin(x[i]))
+	plot(x,y,121,31)
+
+if __name__=="__main__":
+	print_sine()
