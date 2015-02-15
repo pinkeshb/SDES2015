5cf9b686ae1002567e5061f2edada7205fe96022
Pinkesh Barsopia <barsopia@gmail.com>
2015-02-15 21:19:10 +0530

    changing name rastering(), adding code to rastering(), test_rastering and  init_blank_plot...
    changing name of ploting() to rastering(),
    Adding few more test cases to test_rastering()
    Adding code to rastering() and init_blank_plot_with_axix() to clear test cases

--- 123079006/a2/test_text_plot.py
+++ 123079006/a2/test_text_plot.py
@@ -1,100 +1,51 @@
-import test_plot
+import text_plot
 
 def test_init_blank_plot_with_axis():
-	assert "".join(init_blank_plot_with_axis(80,30))=="""                                       |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-   ---------------------------------------|----------------------------------------
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        
-                                          |                                        ""","init_blank_plot_with_axis(80,30) improper"
-
-   assert "".join(init_blank_plot_with_axis(121,31))=="""                                                            |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-   ------------------------------------------------------------+------------------------------------------------------------
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            
-                                                               |                                                            ""","init_blank_plot_with_axis(121,31) improper"
+   assert text_plot.init_blank_plot_with_axis(80,30)==([' ']*39+['|']+[' ']*40+['\n'])*14  + (['-']*39+['+']+['-']*40+['\n']) +([' ']*39+['|']+[' ']*40+['\n'])*15,"init_blank_plot_with_axis(80,30) improper"
+   assert text_plot.init_blank_plot_with_axis(121,31)==([' ']*60+['|']+[' ']*60+['\n'])*15  + (['-']*60+['+']+['-']*60+['\n']) +([' ']*60+['|']+[' ']*60+['\n'])*15,"init_blank_plot_with_axis(121,31) improper"
 
 def test_autorange_data(x,y,X_range,Y_range):
    pass
 
 
-def test_ploting():
-   assert ploting([],[],init_blank_plot_with_axis(80,30)) == init_blank_plot_with_axis(80,30),"ploting() failed no data plot"
- 
-   plotlist = init_blank_plot_with_axis(80,30)
+def test_rastering():
+   assert text_plot.rastering([],[],80,30,text_plot.init_blank_plot_with_axis(80,30)) == text_plot.init_blank_plot_with_axis(80,30),"rastering() failed no data plot"
+   
+   plotlist = text_plot.init_blank_plot_with_axis(80,30)
+   plotlist[0]='*'
+   assert text_plot.rastering([-39],[14],80,30,text_plot.init_blank_plot_with_axis(80,30)) == plotlist,"rastering() failed -39,14 plot"
+
+   plotlist = text_plot.init_blank_plot_with_axis(80,30)
    plotlist[14*81+40-1]='*'
-   assert ploting([0],[0],init_blank_plot_with_axis(80,30)) == plotlist,"ploting() failed origin plot"
+   assert text_plot.rastering([0],[0],80,30,text_plot.init_blank_plot_with_axis(80,30)) == plotlist,"rastering() failed origin plot"
 
-   plotlist = init_blank_plot_with_axis(11,11)
+   plotlist = text_plot.init_blank_plot_with_axis(11,11)
    plotlist[0*12+11-1]='*'
-   assert ploting([5],[5],init_blank_plot_with_axis(11,11)) == plotlist,"ploting() failed non-origin plot"
+   assert text_plot.rastering([5],[5],11,11,text_plot.init_blank_plot_with_axis(11,11)) == plotlist,"rastering() failed non-origin plot"
 
-   plotlist = init_blank_plot_with_axis(11,11)
+   plotlist = text_plot.init_blank_plot_with_axis(11,11)
    plotlist[5*12+6-1]='*'
    plotlist[5*12+11-1]='*'
-   assert ploting([0,5],[0,0],init_blank_plot_with_axis(11,11)) == plotlist,"ploting() failed multi-point plot"
+   assert text_plot.rastering([0,5],[0,0],11,11,text_plot.init_blank_plot_with_axis(11,11)) == plotlist,"rastering() failed multi-point plot"
 
+   plotlist = text_plot.init_blank_plot_with_axis(11,11)
+   plotlist[5*12+11-1]='*'
+   assert text_plot.rastering([4.4999999999999999999999999999999999999999999999999999999999999999],[0],11,11,text_plot.init_blank_plot_with_axis(11,11)) == plotlist,"rastering() failed floating-point plot"
 
+   plotlist = text_plot.init_blank_plot_with_axis(11,11)
+   plotlist[5*12+6-1]='*'
+   plotlist[5*12+10-1]='*'
+   assert text_plot.rastering([0,4.3333333],[0,0],11,11,text_plot.init_blank_plot_with_axis(11,11)) == plotlist,"rastering() failed multi-floating-point plot"
 
 def test_plot(x,y,X_range,Y_range):
    pass
    #test for default size and nothing to plot
 
-#test for default size and ploting single point
+#test for default size and rastering single point
 
-#test for default size and ploting multiple points
+#test for default size and rastering multiple points
 
-#test for default size and ploting out of orfer points
+#test for default size and rastering out of orfer points
 
 #test for cahnge dim and all of the above if needed??
 
@@ -127,9 +78,10 @@ def test_plot(x,y,X_range,Y_range):
 
 def test_text_plot():
    test_init_blank_plot_with_axis()
-   test_ploting()
+   test_rastering()
    #test_autorange_data()
    #test_plot()
+   print "Good JOB"
 
-	
+test_text_plot()
 
--- 123079006/a2/text_plot.py
+++ 123079006/a2/text_plot.py
@@ -1,8 +1,26 @@
 def init_blank_plot_with_axis(X_range,Y_range):
-	pass
+	if X_range%2 == 1:
+		blank_line=[' ']*(X_range/2)+['|']+[' ']*(X_range/2)+['\n']
+		blank_line_middle=['-']*(X_range/2)+['+']+['-']*(X_range/2)+['\n']
+	else:
+		blank_line=[' ']*((X_range-1)/2)+['|']+[' ']*(X_range/2)+['\n']
+		blank_line_middle=['-']*((X_range-1)/2)+['+']+['-']*(X_range/2)+['\n']	
+	if Y_range%2 == 1:
+		blank_plot_with_axis=blank_line*(Y_range/2)+blank_line_middle+blank_line*(Y_range/2)
+	else:
+		blank_plot_with_axis=blank_line*((Y_range-1)/2)+blank_line_middle+blank_line*(Y_range/2)
+	return blank_plot_with_axis
 def autorange_data(x,y,X_range,Y_range):
 	pass
-def ploting(x_scaled,y_scaled):
-	pass
+def rastering(x_scaled,y_scaled,X_range,Y_range,plot_list):
+	no_of_point=len(x_scaled)
+	if no_of_point==0:
+		return plot_list
+	for i in range(0,no_of_point):
+		column=int(round(x_scaled[i]-1+(X_range+1)/2))
+		row=int(round(-y_scaled[i]-1+(Y_range+1)/2))
+		plot_list[row*(X_range+1)+column]='*'
+	return plot_list
+
 def plot(x,y,X_range,Y_range):
 	pass
