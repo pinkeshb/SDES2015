149097fa6584f4e68014629631c800b91fccfc64
Pinkesh Barsopia <barsopia@gmail.com>
2015-02-16 12:14:07 +0530

    Removing redundency form gcd()
    combining 2 ValueError sanity check in 1

--- 123079006/a1/gcd.py
+++ 123079006/a1/gcd.py
@@ -1,11 +1,11 @@
 def gcd(a,b):
 #sanity checks
-	if a<0:
-		raise ValueError
-	if b<0:
+	if a<0 or b<0:
 		raise ValueError
 	if not(type(a) in [int ,   long ] and type(b) in [int, long ]):
 		raise TypeError
+
+#GCD calculation
 	if a == 0:
 		return b
 	while b != 0:
