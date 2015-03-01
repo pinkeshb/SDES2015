1386877d6b7e868df292639568c23b58fac5ab47
Pinkesh B <barsopia@gmail.com>
2015-03-01 22:46:24 +0530

    Adding unittest classes for testing both class and plot function  \n    Adding Test_Plot_class(unittest.TestCase):

---
+++ 123079006/a3/test_text_plot.py
@@ -0,0 +1,8 @@
+import unittest	
+import text_plot
+class Test_Plot_class(unittest.TestCase):
+	pass
+class Test_Points_class(unittest.TestCase):
+	pass
+class Test_plot_function(unittest.TestCase):
+	pass
